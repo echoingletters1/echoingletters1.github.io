2a63bb684024df7221088652cbbb05ee0e81ea52
echoingletters1 <echoingletters123@yahoo.com>
2025-04-19 06:52:57 -0400

    Заливаем index.html

---
+++ policy.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="Privacy Policy">
+    <title>Privacy Policy | E.C.S. Electrical Cable Supply</title>
+    <link rel="stylesheet" href="style.css" media="screen">
+<link rel="stylesheet" href="policy.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i">
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "E.C.S. Electrical Cable Supply",
+		"logo": "images/v41b37.jpg"
+}</script>
+  </head>
+  <body data-path-to-root="./" data-include-products="false" class="u-body u-xl-mode" data-lang="en"><header class="u-clearfix u-header u-header" id="sec-981e"><div class="u-clearfix u-sheet u-sheet-1">
+        <nav class="u-align-left u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse">
+            <a class="u-button-style u-custom-text-color u-nav-link" href="#">
+              <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 302 302" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-0b31"></use></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="svg-0b31" x="0px" y="0px" viewBox="0 0 302 302" style="enable-background:new 0 0 302 302;" xml:space="preserve" class="u-svg-content"><g><rect y="36" width="302" height="30"></rect><rect y="236" width="302" height="30"></rect><rect y="136" width="302" height="30"></rect>
+</g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-black u-text-custom-color-19 u-text-hover-palette-5-dark-3" href="https://electricalcablesupply.com/" style="padding: 10px 20px;">E.C.S. Electrical Cable Supply Ltd.</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-black u-text-custom-color-19 u-text-hover-palette-5-dark-3" href="https://electricalcablesupply.com/#sec-6e7f" style="padding: 10px 20px;">About Us</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-black u-text-custom-color-19 u-text-hover-palette-5-dark-3" href="https://electricalcablesupply.com/#sec-15eb" style="padding: 10px 20px;">Contacts</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-black u-text-custom-color-19 u-text-hover-palette-5-dark-3" href="https://electricalcablesupply.com/#sec-c71f" style="padding: 10px 20px;">Reviews</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-align-center u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="https://electricalcablesupply.com/" style="padding: 10px 20px;">E.C.S. Electrical Cable Supply Ltd.</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="https://electricalcablesupply.com/#sec-6e7f" style="padding: 10px 20px;">About Us</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="https://electricalcablesupply.com/#sec-15eb" style="padding: 10px 20px;">Contacts</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="https://electricalcablesupply.com/#sec-c71f" style="padding: 10px 20px;">Reviews</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <a href="https://electricalcablesupply.com/" class="u-image u-logo u-image-1" data-image-width="399" data-image-height="347">
+          <img src="images/v41b37.jpg" class="u-logo-image u-logo-image-1">
+        </a>
+      </div></header>
+    <section class="u-clearfix u-white u-section-1" id="sec-366e">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-text u-text-1"> E.C.S. Electrical Cable Supply | Privacy Policy</h1>
+        <p class="u-text u-text-2"> At this site, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that collected and recorded on the site and how we use it. If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us. This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in the site. This policy is not applicable to any information collected offline or via channels other than this website.<br>
+          <br>
+          <span style="font-weight: 700;"> Consent<br>
+          </span> By using our website, you agree to our Privacy Policy and agree to its terms.<br>
+          <br>
+          <span style="font-weight: 700;"> Log files<br>
+          </span> Our site follows a standard procedure of using log files. These files record visitors when they visit websites. All hosting companies do this and are part of hosting service analysts. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. They are not linked to any personally identifiable information. The purpose of the information is to analyze trends, administer the site, track users' movements around the site, and gather demographic information.<br>
+          <br>
+          <span style="font-weight: 700;"> CCPA Privacy Rights<br>
+          </span> Under the CCPA, among other rights, California consumers have the right to:<br>
+          <br>Request that a business that collects consumers' personal data disclose the categories and specific pieces of personal data that the company has collected about consumers.<br>
+          <br>Request that a business delete any personal data it has collected about the consumer.<br>
+          <br>
+          <span style="font-weight: 700;"> Data protection rights GDPR<br>
+          </span> We would like to ensure that you are fully aware of all your data protection rights. Each user has the right to the following:<br>
+          <br>Right of access - You have the right to request copies of your personal data. We may charge you a small fee for this service.<br>
+          <br>Right to Correction - You have the right to request that we correct any information that you believe is inaccurate. You also have the right to request that we complete information that you believe is incomplete.<br>
+          <br>Right to erasure - You have the right to request that we erase your personal data under certain conditions.<br>
+          <br>Right to restriction of processing - You have the right to request that we restrict the processing of your personal data under certain conditions.<br>
+          <br>Right to object to processing - You have the right to object to our processing of your personal data under certain conditions.<br>
+          <br>Right to data portability - You have the right to request that we transfer the data we have collected to another organization or directly to you, under certain conditions.<br>
+          <br>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.<br>
+          <br>
+          <span style="font-weight: 700;"> Changes to this Privacy Policy<br>
+          </span> We may update our Privacy Policy from time to time. Therefore, we advise you to periodically review this page for any changes. We will notify you of any changes by posting a new Policy privacy on this page. These changes are effective immediately upon posting on this page.<br>
+          <br>
+          <span style="font-weight: 700;"> Contact us<br>
+          </span> If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.
+        </p>
+      </div>
+    </section>
+    
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-white u-footer" id="sec-19bd"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1"> Copyright 2024. All rights reserved</p>
+      </div></footer>
+  
+</body></html>
